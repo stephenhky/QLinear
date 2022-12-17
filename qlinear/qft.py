fb29e4e1fbd8164f306bf7e34ec8990bd8b2da45
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-12-16 23:19:09 -0500

    debugged, and added invQFT

--- qlinear/qft.py
+++ qlinear/qft.py
@@ -12,4 +12,19 @@ def qft_gate(nbdigits):
         circuit.h(qr[i])
         for j in range(i+1, nbdigits):
             circuit.cp(2*pi/2**(j-i+1), j, i)
+    for i in range(nbdigits // 2):
+        circuit.swap(i, nbdigits-i-1)
+    return circuit.to_gate()
+
+
+def inv_qft_gate(nbdigits):
+    qr = QuantumRegister(nbdigits, 'qr')
+    circuit = QuantumCircuit(qr)
+    for i in range(nbdigits // 2):
+        circuit.swap(i, nbdigits-i-1)
+    for i in range(nbdigits):
+        circuit.h(qr[i])
+        for j in range(i+1, nbdigits):
+            rot_phase = - 2*pi/2**(j-i+1)
+            circuit.cp(rot_phase, j, i)
     return circuit.to_gate()
