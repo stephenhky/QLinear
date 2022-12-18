7f9cfa48d0fe7d5cca040fa98f91420bbc40b645
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-12-18 18:20:23 -0500

    modularized phase estimation gate

--- qlinear/qft.py
+++ qlinear/qft.py
@@ -28,3 +28,28 @@ def inv_qft_gate(nbdigits):
             rot_phase = - 2*pi/2**(j-i+1)
             circuit.cp(rot_phase, j, i)
     return circuit.to_gate()
+
+
+def phase_estimation_circuit(gate, nbdigits, initial_momopartite_state=[0, 1]):
+    # customized controlled gate
+    controlled_gate = gate.control(1)
+    # initial_state
+    assert len(initial_momopartite_state) == 2
+    assert np.linalg.norm(initial_momopartite_state) == 1
+    initial_state = np.zeros(2**(nbdigits+1), dtype=np.complex_)
+    initial_state[0] = initial_momopartite_state[0]
+    initial_state[2**nbdigits] = initial_momopartite_state[1]
+
+    # building the circuit
+    circuit = QuantumCircuit(nbdigits + 1, nbdigits)
+    circuit.initialize(initial_state)
+    circuit.h(range(nbdigits))
+    for i in range(nbdigits):
+        for _ in range(2 ** (i)):
+            circuit.append(controlled_gate, [i, nbdigits])
+            # circuit.cp(2*pi*theta, i, nbdigits)
+    circuit.append(inv_qft_gate(nbdigits), range(nbdigits))
+    circuit.measure(range(nbdigits), range(nbdigits))
+
+    return circuit
+
